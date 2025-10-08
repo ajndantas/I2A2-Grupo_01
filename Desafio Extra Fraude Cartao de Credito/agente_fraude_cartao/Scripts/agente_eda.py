c2c2233217bca2fd8acb1b030a4f01c857478277
Antonio Dantas <antonio.dantas@gmail.com>
2025-10-08 12:48:55 -0300

    repositorio

--- Desafio Extra Fraude Cartao de Credito/agente_fraude_cartao/Scripts/agente_eda.py
+++ Desafio Extra Fraude Cartao de Credito/agente_fraude_cartao/Scripts/agente_eda.py
@@ -586,7 +586,7 @@ if __name__ == "__main__":
         #model="tngtech/deepseek-r1t2-chimera:free",
         model="x-ai/grok-4-fast",
         base_url="https://openrouter.ai/api/v1",
-        temperature=0.4,
+        temperature=0.3,
         cache=True,        
         reasoning_effort="high",                
         api_key=getenv("API_KEY")        
