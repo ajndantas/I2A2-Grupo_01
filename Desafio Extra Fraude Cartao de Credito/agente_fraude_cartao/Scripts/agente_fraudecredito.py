496e2032a491392284ff7c12de5ad05201033abd
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2025-10-08 08:37:55 -0300

    Agente cartao credito fraude

--- Desafio Extra Fraude Cartao de Credito/agente_fraude_cartao/Scripts/agente_fraudecredito.py
+++ Desafio Extra Fraude Cartao de Credito/agente_fraude_cartao/Scripts/agente_fraudecredito.py
@@ -396,7 +396,7 @@ def agente1(llm:ChatOpenAI, engine:Engine, conclusoes:List[Dict[str,str]],qtd_to
     if 'conclusoes_done' not in st.session_state:
         st.session_state['conclusoes_done'] = conclusoes
                
-    st.markdown('<a href="https://drive.google.com/open?id=16sr_ytwwHYKu0jfC5BuCj3NbMReSLoHv&usp=drive_fs" target="_blank">Ex: Arquivo de dados</a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://github.com/ajndantas/I2A2-Grupo_01/blob/master/Desafio%20Extra%20Fraude%20Cartao%20de%20Credito/agente_fraude_cartao/Scripts/creditcard.zip" target="_blank">Ex: Arquivo de dados</a>', unsafe_allow_html=True)
     uploaded_file = st.file_uploader("📂 Carregue o arquivo csv de dados", type=["csv"])        
     default_index = 0
     
