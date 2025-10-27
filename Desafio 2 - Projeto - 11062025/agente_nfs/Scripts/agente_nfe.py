1fcbd6f393726cd56f8437ed39b81e123416fbd4
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2025-10-26 21:49:35 -0300

    Testes Agente NFE

--- Desafio 2 - Projeto - 11062025/agente_nfs/Scripts/agente_nfe.py
+++ Desafio 2 - Projeto - 11062025/agente_nfs/Scripts/agente_nfe.py
@@ -64,7 +64,7 @@ def consultallmdocfiscal(texto,llm,tipo):
         b) Manual de Orientação do Contribuinte (MOC) 
         c) Schemas XSD referentes ao documento fiscal. Para impostos, identifiquem quais estão no documentos fiscal por meio das tags.
         d) Sobre impostos, consultar os itens b) e c). 
-        3 - Verificar os significados encontrados no item 2, ** SE HOUVER ALGUM REPETIDO, RETORNAR PARA O ITEM 2 **
+        3 - Liste todos os significados gerados do item 2, ** SEMPRE ** que houver algum ** REPETIDO **, adicionar a palavra PRESTADOR OU TOMADOR, dependendo do significado.
         4 - Para cada campo deve-se ter o respectivo valor
         5 - Baseados nos campos do item 2 e na sigla do item 1. Qual é a versão desse documento fiscal ? Caso não encontre, procurar na legislação. Responda somente com o número da versão. 
         6 - Baseados nos campos do item 2 e na sigla do item 1. Qual é o número do modelo desse documento fiscal ? Caso não encontre, procurar na legislação. Responda somente com o número do modelo.
