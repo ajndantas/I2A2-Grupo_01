1b41f386cfd7378f82d9ac81ce76bab2c10adc08
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2025-10-27 00:31:14 -0300

    Testes Agente NFE

--- Desafio 2 - Projeto - 11062025/agente_nfs/Scripts/agente_nfe.py
+++ Desafio 2 - Projeto - 11062025/agente_nfs/Scripts/agente_nfe.py
@@ -71,7 +71,8 @@ def consultallmdocfiscal(texto,llm,tipo):
         d) Sobre impostos, consultar os itens b) e c). 
         
         3 - Liste todos os significados gerados do item 2, ** SEMPRE ** que houver algum ** REPETIDO **, adicionar a palavra PRESTADOR OU TOMADOR, dependendo do significado.
-        4 - Para cada significado do item 2, identificar o valor em CONTEÚDO. 
+        4 - Para cada significado do item 2, identificar o valor em CONTEÚDO, criando um dicionário {{"SIGNIFICADO":"VALOR"}} chamado DICIONARIO
+        4.1 - Verificar em DICIONARIO, se o VALOR associado a cada SIGNIFICADO está correto, se não, retornar para o passo 4, até que cada VALOR associado ao seu SIGNIFICADO esteja correto.
         5 - Baseados nos campos do item 2 e na sigla do item 1. Qual é a versão desse documento fiscal ? Caso não encontre, procurar na legislação. Responda somente com o número da versão. 
         6 - Baseados nos campos do item 2 e na sigla do item 1. Qual é o número do modelo desse documento fiscal ? Caso não encontre, procurar na legislação. Responda somente com o número do modelo.
         ###########################################
