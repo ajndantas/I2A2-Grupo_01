8b1af2548170befdd84b16f6e92402b5c7601ab0
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-05-09 17:26:52 -0300

    agente chatbotseguro

--- Insurminds/Desafio 2/agente_chatbotseguro/Scripts/agente_chatbotseguro.py
+++ Insurminds/Desafio 2/agente_chatbotseguro/Scripts/agente_chatbotseguro.py
@@ -10,7 +10,7 @@
 
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from os import getenv, environ
+from os import getenv, path
 from langchain_core.globals import set_debug, set_llm_cache
 from langchain_core.caches import InMemoryCache
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
@@ -20,9 +20,9 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 
-# set_debug(True)
+#set_debug(True)
 
-load_dotenv() # CARREGANDO O ARQUIVO COM A OPENAI_KEY
+load_dotenv() # CARREGANDO O ARQUIVO COM A API_KEY_OPENROUTER
 
 # -------------------------------------------------------
 # Cache nativo do LangChain (InMemoryCache)
@@ -96,13 +96,26 @@ class SearchIndex:
 # PASSO 3 - BANCO DE VETORES - UTILIZAR O ÍNDICE DE BUSCA PARA ARMAZENAR OS CHUNKS E SEUS RESPECTIVOS VETORES DE EMBEDDING.
 class VectorDB:
  
-    def __init__(self, documents: list, embeddings):
+    def __init__(self, documents: list, embeddings, db_path: str):
         self.documents = documents
         self.embeddings = embeddings
 
-    def db(self) -> FAISS:        
+        self.db_path = db_path # O CAMINHO ONDE O ÍNDICE DE BUSCA VAI SER ARMAZENADO. 
+                               # O OBJETIVO DE ARMAZENAR O ÍNDICE EM UM ARQUIVO É PERMITIR QUE ELE SEJA REUTILIZADO 
+                               # EM VEZ DE SER RECRIADO TODA VEZ QUE O PROGRAMA FOR EXECUTADO AUMENTANDO ASSIM A PERFORMANCE.
+
+    def db(self) -> FAISS:
+
+        if path.exists(self.db_path):
+            return FAISS.load_local(self.db_path, self.embeddings, allow_dangerous_deserialization=True) # O MÉTODO LOAD_LOCAL VAI CARREGAR O ÍNDICE DE BUSCA A PARTIR DO ARQUIVO SALVO. 
+                                                                                                         # O PARÂMETRO ALLOW_DANGEROUS_DESERIALIZATION É NECESSÁRIO PARA PERMITIR 
+                                                                                                         # QUE O FAISS CARREGUE O ÍNDICE DE BUSCA A PARTIR DO ARQUIVO        
+        
+        # Se não existir, cria e salva
         db_instance = FAISS.from_documents(self.documents, self.embeddings)
-                    
+        
+        db_instance.save_local(self.db_path)
+
         return db_instance
 
 
@@ -113,19 +126,29 @@ class AgenteChatbotSeguro:
     llm = ChatOpenAI(
                         model="openrouter/free",                    
                         api_key=getenv("API_KEY_OPENROUTER"),
-                        base_url="https://openrouter.ai/api/v1"                    
+                        base_url="https://openrouter.ai/api/v1",
+                        temperature=0 # POR CAUSA DE PERGUNTAS COMO "QUEM DESCOBRIU O BRASIL ?", É IMPORTANTE DEFINIR A TEMPERATURA COMO 0 
+                                      # PARA GARANTIR QUE A RESPOSTA SEJA SEMPRE A MESMA E NÃO VARIE ENTRE EXECUÇÕES DIFERENTES.                   
                     )
     
     documents = Loader().load() # PASSO 1 - DIVIDIR OS DOCUMENTOS EM CHUNKS (FRAGMENTOS) PARA FACILITAR O PROCESSAMENTO PELA LLM. O CHUNK_SIZE VAI DETERMINAR O TAMANHO DE CADA FRAGMENTO.
     searchindex = SearchIndex(chunk_size=1000, documents=documents) # PASSO 2 - CRIAR UM ÍNDICE DE BUSCA PARA OS CHUNKS GERADOS. ESSE ÍNDICE VAI PERMITIR REALIZAR BUSCAS EFICIENTES NOS DOCUMENTOS FRAGMENTADOS.
-    db = VectorDB(documents=searchindex.splitter(), embeddings=searchindex.indexer()).db() # PASSO 3 - BANCO DE VETORES - UTILIZAR O ÍNDICE DE BUSCA PARA ARMAZENAR OS CHUNKS E SEUS RESPECTIVOS VETORES DE EMBEDDING.
+    
+    self.db_path = "faiss_index"
+
+    if path.exists(self.db_path): 
+        print("Apagando Índice de busca existente para carga de novos documentos...")
+        path.remove(self.db_path) # APAGANDO O ÍNDICE DE BUSCA EXISTENTE PARA GARANTIR QUE O NOVO ÍNDICE SEJA CRIADO COM OS NOVOS DOCUMENTOS CARREGADOS. 
+
+    db = VectorDB(documents=searchindex.splitter(), embeddings=searchindex.indexer(), db_path=self.db_path).db() # PASSO 3 - BANCO DE VETORES - UTILIZAR O ÍNDICE DE BUSCA PARA ARMAZENAR OS CHUNKS E SEUS RESPECTIVOS VETORES DE EMBEDDING.
 
     # create the RetrievalQA chain using the existing llm and the retriever (Quem busca no banco de dados)
     # qa_chain -> Nossa ferramenta de Perguntas e Respostas (Questions and Answers Chain)
     self.qa_chain = RetrievalQA.from_chain_type(
                                                   llm=llm, 
                                                   retriever=db.as_retriever(),
-                                                  return_source_documents=True                       
+                                                  return_source_documents=True # ESSA OPÇÃO VAI PERMITIR QUE A CHAINE DEVOLVA OS DOCUMENTOS DE ORIGEM QUE FORAM UTILIZADOS PARA GERAR A RESPOSTA.
+                                                                                                                                                                             
                                                 )
     
   def query(self, question: str) -> str:
@@ -134,7 +157,6 @@ class AgenteChatbotSeguro:
       self.result = self.output['result']
 
       print('\nPergunta: ', question)
-      print('Documentos de origem da resposta:\n', [doc.metadata for doc in self.output['source_documents']])
 
       return self.result
 
@@ -143,7 +165,7 @@ if __name__ == "__main__":
 
     agente = AgenteChatbotSeguro()
 
-    print(agente.query("Aonde consultar as licitações das unidades da Susep ?"))
+    print(agente.query("Não encontrei um seguro que eu contratei. O que fazer?"))
     print(agente.query("Como devo proceder caso tenha um item pessoal roubado ?"))
     print(agente.query("Quem descobriu o Brasil ?"))
 
