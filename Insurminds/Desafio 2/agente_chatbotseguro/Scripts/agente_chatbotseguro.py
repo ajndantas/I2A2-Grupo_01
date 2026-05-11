18865ec22c881fa8619bb5bebd6dd120681e0357
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-05-10 23:55:44 -0300

    agente)chatbotseguro

--- Insurminds/Desafio 2/agente_chatbotseguro/Scripts/agente_chatbotseguro.py
+++ Insurminds/Desafio 2/agente_chatbotseguro/Scripts/agente_chatbotseguro.py
@@ -9,13 +9,15 @@
 # 3 - EXECUTANDO A PESQUISA
 
 
+from tabnanny import verbose
+
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from os import getenv, path
-from langchain_core.globals import set_debug, set_llm_cache
+from langchain_core.globals import set_debug, set_llm_cache, set_verbose
 from langchain_core.caches import InMemoryCache
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
@@ -24,8 +26,8 @@ from langchain_core.prompts import PromptTemplate
 from time import time
 from re import sub
 
-
 #set_debug(True)
+#set_verbose(True)
 
 load_dotenv() # CARREGANDO O ARQUIVO COM A API_KEY_OPENROUTER
 
@@ -58,26 +60,39 @@ class Loader:
 # PASSO 2 - CRIAÇÃO DO ÍNDICE DE BUSCA
 #
 # Para isso, será necessário, primeiramente, realizar a quebra (splitter) em trechos, para que a IA possa indexá-los.
-
 class SearchIndex:
 
-    def __init__(self, chunk_size: int, chunk_overlap: int, documents: list):
+    # 1 - DIVIDIR OS DOCUMENTOS EM CHUNKS (FRAGMENTOS) DE TEXTO PARA FACILITAR O PROCESSAMENTO PELA LLM. O CHUNK_SIZE VAI DETERMINAR O TAMANHO DE CADA FRAGMENTO.
+    def splitter(self, chunk_size: int, chunk_overlap: int, documents: list) -> list:
 
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
-        self.documents = documents
-    
-    # PASSO 1 - DIVIDIR OS DOCUMENTOS EM CHUNKS (FRAGMENTOS) DE TEXTO PARA FACILITAR O PROCESSAMENTO PELA LLM. O CHUNK_SIZE VAI DETERMINAR O TAMANHO DE CADA FRAGMENTO.
-    def splitter(self) -> list:
-        
-        splitter = CharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap) # O CHUNK_OVERLAP VAI DETERMINAR O NÍVEL DE SOBREPOSIÇÃO ENTRE OS CHUNKS. SE FOR 0, NÃO HAVERÁ SOBREPOSIÇÃO. 
-                                                                                      # SE FOR MAIOR QUE 0, OS CHUNKS VÃO SE SOBREPOR EM UMA QUANTIDADE DE CARACTERES DETERMINADA 
-                                                                                      # PELO VALOR DO CHUNK_OVERLAP.
-        self.texts_chunks = splitter.split_documents(self.documents)  
+        splitter = RecursiveCharacterTextSplitter(                                                    
+                                                    chunk_size=chunk_size, 
+                                                    chunk_overlap=chunk_overlap,                                                    
+                                                    separators=["</h1>","</h2>","</h3>","</h4>","</h5>","\n\n", "\n", " ", ""], # OS SEPARADORES VÃO DETERMINAR ONDE O 
+                                                                                                                                # SPLITTER VAI TENTAR QUEBRAR O TEXTO. 
+                                                                                                                                # ELE VAI TENTAR QUEBRAR PRIMEIRO 
+                                                                                                                                # PELO SEPARADOR MAIS PRIORITÁRIO, 
+                                                                                                                                # E SE NÃO CONSEGUIR, VAI TENTAR PELO PRÓXIMO 
+                                                                                                                                # SEPARADOR DA LISTA.
 
-        return self.texts_chunks
+                                                                                                                                # Somente se o texto entre as tags ainda for maior que o seu chunk_size 
+                                                                                                                                # é que o splitter utilizará os demais 
+                                                                                                                                # separadores (\n\n, \n, etc.)
+                                                                                                                                #
+                                                                                                                                # O ÚLTIMO SEPARADOR É UMA STRING VAZIA, 
+                                                                                                                                # O QUE SIGNIFICA QUE O SPLITTER VAI TENTAR 
+                                                                                                                                # QUEBRAR O TEXTO EM QUALQUER LUGAR SE NÃO 
+                                                                                                                                # CONSEGUIR PELO SEPARADOR ANTERIOR.
+                                                    verbose=True
+                                                  ) # O CHUNK_OVERLAP VAI DETERMINAR O NÍVEL DE SOBREPOSIÇÃO ENTRE OS CHUNKS. SE FOR 0, NÃO HAVERÁ SOBREPOSIÇÃO. 
+                                                    # SE FOR MAIOR QUE 0, OS CHUNKS VÃO SE SOBREPOR EM UMA QUANTIDADE DE CARACTERES DETERMINADA 
+                                                    # PELO VALOR DO CHUNK_OVERLAP.
+
+        self.chunked_docs = splitter.split_documents(documents)  
+
+        return self.chunked_docs 
     
-    # PASSO 2 - CRIAR UM ÍNDICE DE BUSCA PARA OS CHUNKS GERADOS. ESSE ÍNDICE VAI PERMITIR REALIZAR BUSCAS EFICIENTES NOS DOCUMENTOS FRAGMENTADOS.
+    # 2 - CRIAR UM ÍNDICE DE BUSCA PARA OS CHUNKS GERADOS E SEUS RESPECTIVOS VETORES DE EMBEDDING. ESSE ÍNDICE VAI PERMITIR REALIZAR BUSCAS NOS DOCUMENTOS FRAGMENTADOS.
     def indexer(self) -> HuggingFaceEmbeddings:
 
         self.embeddings = HuggingFaceEmbeddings(
@@ -99,16 +114,16 @@ class SearchIndex:
         
         return self.embeddings
 
-# PASSO 3 - BANCO DE VETORES - UTILIZAR O ÍNDICE DE BUSCA PARA ARMAZENAR OS CHUNKS E SEUS RESPECTIVOS VETORES DE EMBEDDING.
+# PASSO 3 - BANCO DE VETORES - PARA ARMAZENAR O ÍNDICE DE BUSCA DOS CHUNKS DOS DOCS E SEUS RESPECTIVOS VETORES DE EMBEDDING.
 class VectorDB:
  
-    def __init__(self, documents: list, embeddings, db_path: str):
+    def __init__(self, documents: list, embeddings, db_path_name: str):
         self.documents = documents
         self.embeddings = embeddings
 
-        self.db_path = db_path # O CAMINHO ONDE O ÍNDICE DE BUSCA VAI SER ARMAZENADO. 
-                               # O OBJETIVO DE ARMAZENAR O ÍNDICE EM UM ARQUIVO É PERMITIR QUE ELE SEJA REUTILIZADO 
-                               # EM VEZ DE SER RECRIADO TODA VEZ QUE O PROGRAMA FOR EXECUTADO AUMENTANDO ASSIM A PERFORMANCE.
+        self.db_path = db_path_name # O CAMINHO ONDE O ÍNDICE DE BUSCA VAI SER ARMAZENADO. 
+                                         # O OBJETIVO DE ARMAZENAR O ÍNDICE EM UM ARQUIVO É PERMITIR QUE ELE SEJA REUTILIZADO 
+                                         # EM VEZ DE SER RECRIADO TODA VEZ QUE O PROGRAMA FOR EXECUTADO AUMENTANDO ASSIM A PERFORMANCE.
 
     def db(self) -> FAISS:
 
@@ -120,7 +135,7 @@ class VectorDB:
         
         # Se não existir, cria e salva
         print("Índice de busca não encontrado. Criando...\n")
-        db_instance = FAISS.from_documents(self.documents, self.embeddings)
+        db_instance = FAISS.from_documents(self.documents, self.embeddings) # CRIANDO O BD DE VETORES A PARTIR DOS DOCUMENTOS E DOS VETORES DE EMBEDDING. 
         
         db_instance.save_local(self.db_path)
 
@@ -132,24 +147,32 @@ class AgenteChatbotSeguro:
   def __init__(self):
 
     llm = ChatOpenAI(
-                        model="openrouter/free",                    
-                        api_key=getenv("API_KEY_OPENROUTER"),
-                        base_url="https://openrouter.ai/api/v1",
-                        reasoning_effort="high"                  
+                        #model="openrouter/free",
+                        model="gpt-5-mini",                   
+                        #api_key=getenv("API_KEY_OPENROUTER"),
+                        api_key=getenv("API_KEY"),
+                        reasoning_effort="high",
+                        #base_url="https://openrouter.ai/api/v1"                  
                     )
     
-    documents = Loader().load() # PASSO 1 - DIVIDIR OS DOCUMENTOS EM CHUNKS (FRAGMENTOS) PARA FACILITAR O PROCESSAMENTO PELA LLM. O CHUNK_SIZE VAI DETERMINAR O TAMANHO DE CADA FRAGMENTO.
-    searchindex = SearchIndex(chunk_size=800, chunk_overlap=500, documents=documents) # PASSO 2 - CRIAR UM ÍNDICE DE BUSCA PARA OS CHUNKS GERADOS. ESSE ÍNDICE VAI PERMITIR REALIZAR BUSCAS EFICIENTES NOS DOCUMENTOS FRAGMENTADOS.
-    
-    self.db_path = "faiss_index"
+    documents = Loader().load() # PASSO 1 - CARGA NO CARREGADOR                        
+    # PASSO 2 - CRIAÇÃO DO ÍNDICE DE BUSCA
+    searchindex = SearchIndex()
+    chunked_docs = searchindex.splitter(chunk_size=800, chunk_overlap=500, documents=documents) # PASSO 2.1 - QUEBRA DO TEXTO - DIVIDIR OS DOCUMENTOS 
+                                                                                                # EM CHUNKS (FRAGMENTOS) DE TEXTO PARA FACILITAR O PROCESSAMENTO PELA LLM.
+
+
+    #print("Chunked Docs:\n",chunked_docs)
+
+    db_path_name = "faiss_index"
 
     #if path.exists(self.db_path): 
     #   print("Apagando Índice de busca existente para carga de novos documentos...\n")
     #   shutil.rmtree(self.db_path) # O MÉTODO RMTREE VAI APAGAR O DIRETÓRIO ONDE O ÍNDICE DE BUSCA ESTÁ SALVO, INCLUINDO TODOS OS ARQUIVOS E SUBDIRETÓRIOS CONTIDOS NELE.
-                                    # rmdir só pode ser usado para remover diretórios vazios.       
-         
-
-    db = VectorDB(documents=searchindex.splitter(), embeddings=searchindex.indexer(), db_path=self.db_path).db() # PASSO 3 - BANCO DE VETORES - UTILIZAR O ÍNDICE DE BUSCA PARA ARMAZENAR OS CHUNKS E SEUS RESPECTIVOS VETORES DE EMBEDDING.
+                                    # rmdir só pode ser usado para remover diretórios vazios.
+    db = VectorDB(documents=chunked_docs, embeddings=searchindex.indexer(), db_path_name=db_path_name).db() # PASSO 3 - BANCO DE VETORES - PARA ARMAZENAR O ÍNDICE DE 
+                                                                                                            # BUSCA DOS CHUNKS DOS DOCS E SEUS RESPECTIVOS VETORES 
+                                                                                                            # DE EMBEDDING.
     
     template = """
                     Você é um assistente de perguntas e respostas especializado em seguros.
@@ -163,10 +186,9 @@ class AgenteChatbotSeguro:
                     DIRETRIZES:
                     ------------------------------------------------------------------------------------------------------------------------------------------------------------------
                         - Se os documentos não contiverem informações relevantes para responder à pergunta, responda "Desculpe, não tenho informações suficientes para 
-                        responder a essa pergunta.".
+                        responder a essa pergunta." e deixar em branco as fontes.
                         - Se os documentos contiverem informações relevantes, responda com base nessas informações, citando os nomes das fontes utilizadas. Seja claro e conciso em 
-                        suas respostas.
-                        - Se as fontes não forem citadas, responda "Desculpe, não existem fontes que possam responder a essa pergunta.". NUNCA deixar em branco ou omitir as fontes.
+                        suas respostas.                        
                         - SEMPRE responda no formato JSON, seguindo a estrutura definida abaixo.
                     ------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
                     
@@ -183,7 +205,8 @@ class AgenteChatbotSeguro:
     prompt_template = PromptTemplate(
                                         template=template,
                                         input_variables=["context", "question"]                                  
-                                    )
+                                    )    
+    
 
     # create the RetrievalQA chain using the existing llm and the retriever (Quem busca no banco de dados)
     # 
@@ -203,7 +226,8 @@ class AgenteChatbotSeguro:
   def query(self, question: str) -> str:
       
       self.output = self.qa_chain.invoke({"query": question}) 
-      self.result = sub(r"```json|```","",str(self.output['result'])).strip() # O RESULTADO VAI VIR COM QUEBRAS DE LINHA, ENTÃO SUBSTITUÍMOS AS QUEBRAS DE LINHA POR ESPAÇOS EM BRANCO PARA DEIXAR O JSON EM UMA ÚNICA LINHA.
+      self.result = sub(r"```json|```","",str(self.output['result'])).strip() # O RESULTADO VAI VIR COM QUEBRAS DE LINHA, ENTÃO SUBSTITUÍMOS AS QUEBRAS DE LINHA 
+                                                                              # POR ESPAÇOS EM BRANCO PARA DEIXAR O JSON EM UMA ÚNICA LINHA.
 
       return self.result
 
