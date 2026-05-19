51a07ca2c7cbccddba68fed623e45486f2e11d89
Antonio Dantas <antonio.dantas@gmail.com>
2026-05-19 11:57:30 -0300

    agente chatbotseguro

--- Insurminds/Desafio 2/agente_chatbotseguro/Scripts/agente_chatbotseguro.py
+++ Insurminds/Desafio 2/agente_chatbotseguro/Scripts/agente_chatbotseguro.py
@@ -188,11 +188,11 @@ class AgenteChatbotSeguro:
 
                     DIRETRIZES:
                     ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                        - **SEMPRE** pesquisar nos documentos que sejam relacionados ao bem ou bem de mesmo significado que este bem informado na pergunta. 
-                        Ex: Se o bem informado for celular, pesquisar nos documentos que sejam relacionados a este bem, o mesmo sendo aplicado para bens que tenham o mesmo 
-                        significado que este bem (Ex: smartphone). Caso o bem ou para bens que tenha o mesmo significado que este bem, não esteja relacionado aos documentos, **SEMPRE** responda "Desculpe, não tenho informações 
-                        suficientes para responder a essa pergunta. Entre em contato com um especialista por meio de email, informe no assunto um resumo do problema e também o 
-                        protocolo gerado, para que ele possa lhe ajudar." e nada mais.
+                        - De acordo com o bem presente na PERGUNTA {question} *SEMPRE** pesquisar nos documentos que sejam relacionados ao bem ou que possuam o mesmo significado
+                        deste bem. 
+                        Ex: Se o bem informado for celular, pesquisar nos documentos que sejam relacionados a este bem. Caso o bem não esteja relacionado aos documentos, **SEMPRE** 
+                        responda "Desculpe, não tenho informações suficientes para responder a essa pergunta. Entre em contato com um especialista por meio de email, informe no 
+                        assunto um resumo do problema e também o protocolo gerado, para que ele possa lhe ajudar." e nada mais.
 
                         - Se os documentos não contiverem informações relevantes para responder à pergunta, **SEMPRE** responda "Desculpe, não tenho informações suficientes para
                         responder a essa pergunta. Entre em contato com um especialista por meio de email, informe no assunto um resumo do problema e também o protocolo gerado, para 
