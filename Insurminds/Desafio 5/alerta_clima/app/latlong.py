0d1063323f8be97e1ebb98224c5447251bbc0675
Antonio Dantas <antonio.dantas@gmail.com>
2026-09-02 15:43:54 -0300

    llm desafio 5

--- Insurminds/Desafio 5/alerta_clima/app/latlong.py
+++ Insurminds/Desafio 5/alerta_clima/app/latlong.py
@@ -1,16 +1,10 @@
+from llm import LLM
 from pydantic import BaseModel, Field
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from dotenv import load_dotenv
-from os import getenv
 from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.globals import set_debug, set_llm_cache
 from langchain_core.exceptions import OutputParserException
-from langchain_core.caches import InMemoryCache
+from llm import LLM
 
-set_debug(True)
-
-load_dotenv()
 
 class LatLongModel(BaseModel):
     cidade: str = Field(description="O nome da cidade")
@@ -20,19 +14,8 @@ class LatLongModel(BaseModel):
 class LatLong:
     def __init__(self):
 
-        self.llm = ChatOpenAI(
-                                #model_name="openrouter/free",
-                                #base_url="https://openrouter.ai/api/v1",
-
-                                model_name="gpt-5.6-luna", 
-                                api_key=getenv("API_KEY"),
-                                #api_key=getenv("API_KEY_OPENROUTER"),
-                                #reasoning_effort="high",
-                                temperature=0                            
-                            )        
-
-        set_llm_cache(InMemoryCache()) # Ativando memória personalizada
-
+        self.llm = LLM().getLLM()
+        
         template = """
                         Qual é a latitude e a longitude da cidade {cidade} ?
 
--- Insurminds/Desafio 5/alerta_clima/frontend/tipsandcities.py
+++ Insurminds/Desafio 5/alerta_clima/frontend/tipsandcities.py
@@ -1,32 +1,14 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from dotenv import load_dotenv
-from os import getenv
 from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.globals import set_debug, set_llm_cache
-from langchain_core.caches import InMemoryCache
 from app.modelos.tipsandcities import TipsandCities as TipsandCitiesModel
-
-set_debug(True)
-
-load_dotenv()
+from llm import LLM
 
 class TipsandCities:
     def __init__(self):
 
-        llm = ChatOpenAI(
-                                #model_name="openrouter/free",
-                                #base_url="https://openrouter.ai/api/v1",
-
-                                model_name="gpt-5.6-luna", 
-                                api_key=getenv("API_KEY"),
-                                #api_key=getenv("API_KEY_OPENROUTER"),
-                                #reasoning_effort="high",
-                                temperature=0                            
-                         )        
-
-        set_llm_cache(InMemoryCache()) # Ativando memória personalizada
-
+        llm = LLM().getLLM() 
+        
         template = """
                         Aja como um especialistas de meteorologia e clima. e siga PASSOS abaixo:
 
