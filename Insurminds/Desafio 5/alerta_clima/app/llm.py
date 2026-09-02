68e85930f496049b4dfdea1a6561db5260c67435
Antonio Dantas <antonio.dantas@gmail.com>
2026-09-02 15:57:12 -0300

    llm desafio 5

--- Insurminds/Desafio 5/alerta_clima/app/llm.py
+++ Insurminds/Desafio 5/alerta_clima/app/llm.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from os import getenv
 from langchain_core.globals import set_debug, set_llm_cache
@@ -12,16 +12,21 @@ class LLM:
     def getLLM(self):
 
         self.llm = ChatOpenAI(
-                                        #model_name="openrouter/free",
-                                        #base_url="https://openrouter.ai/api/v1",
+                                #model_name="openrouter/free",
+                                #base_url="https://openrouter.ai/api/v1",
 
-                                        model_name="gpt-5.6-luna", 
-                                        api_key=getenv("API_KEY"),
-                                        #api_key=getenv("API_KEY_OPENROUTER"),
-                                        #reasoning_effort="high",
-                                        temperature=0                            
-                                )
+                                model_name="gpt-5.6-luna", 
+                                api_key=getenv("API_KEY"),
+                                #api_key=getenv("API_KEY_OPENROUTER"),
+                                #reasoning_effort="high",
+                                temperature=0                            
+                            )
 
         set_llm_cache(InMemoryCache()) # Ativando memória personalizada
 
         return self.llm
+
+if __name__ == "__main__":
+
+    llm = LLM()
+    llm.getLLM()
