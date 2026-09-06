b07b6af4c713b7797420d627bf5093b3c5aafa76
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-09-06 20:48:47 -0300

    desafio 5

--- Insurminds/Desafio 5/alerta_clima/app/llm.py
+++ Insurminds/Desafio 5/alerta_clima/app/llm.py
@@ -12,8 +12,7 @@ class LLM:
     def getLLM(self):
 
         self.llm = ChatOpenAI(
-                                model_name="minimax/minimax-m3:free",
-                                #model_name="openrouter/free",
+                                model_name="openrouter/free",
                                 base_url="https://openrouter.ai/api/v1",
 
                                 #model_name="gpt-5.6-luna",
