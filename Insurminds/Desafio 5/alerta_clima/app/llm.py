76bae2682863f5079af966b7d749cc6ed4fac9e7
Antonio Dantas <antonio.dantas@gmail.com>
2026-09-10 16:21:58 -0300

    desafio 5

--- Insurminds/Desafio 5/alerta_clima/app/llm.py
+++ Insurminds/Desafio 5/alerta_clima/app/llm.py
@@ -16,7 +16,7 @@ class LLM:
 
         llm = ChatOpenAI(
                                 #model_name="openrouter/free",
-                                model_name="nvidia/nemotron-3.5-lightning:free",
+                                model_name="dots-studio/dots-3-note-preview:free",
                                 base_url="https://openrouter.ai/api/v1",
 
                                 #model_name="gpt-5.4-mini", 
