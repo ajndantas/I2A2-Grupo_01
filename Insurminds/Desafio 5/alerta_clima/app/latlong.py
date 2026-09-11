104891234b9b37a8d62c4e0695626cb82e9285c5
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-09-11 11:51:42 -0300

    desafio 5

--- Insurminds/Desafio 5/alerta_clima/app/latlong.py
+++ Insurminds/Desafio 5/alerta_clima/app/latlong.py
@@ -25,12 +25,12 @@ class LatLong:
         set_llm_cache(InMemoryCache())
 
         self.llm = ChatOpenAI(
-                                model_name="gpt-5.6-luna",
+                                #model_name="gpt-5.6-luna",
                                 #model_name="gpt-5.4-mini",
-                                #model_name="openrouter/free",
+                                model_name="inclusionai/ling-3.0-flash-fin:free",
                                 #base_url="https://openrouter.ai/api/v1",
-                                #api_key=getenv("API_KEY_OPENROUTER"), 
-                                api_key=getenv("API_KEY"), 
+                                api_key=getenv("API_KEY_OPENROUTER"), 
+                                #api_key=getenv("API_KEY"), 
                                 temperature=0
                               )
 
