2f4e401c122c84f7339e5bc60a94eeff6becdf88
Antonio Dantas <antonio.dantas@gmail.com>
2026-09-15 10:51:12 -0300

    desafio 5

--- Insurminds/Desafio 4/agente_docfiscais/app/main.py
+++ Insurminds/Desafio 4/agente_docfiscais/app/main.py
@@ -18,7 +18,7 @@ allow_origins=[
         "https://ENDERECO-PUBLICADO-DO-FRONTEND"
     ],"""
 
-app.add_middleware(
+""" app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8000",
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) """
 
 @app.get("/", response_class=HTMLResponse)
 async def frontpage():
