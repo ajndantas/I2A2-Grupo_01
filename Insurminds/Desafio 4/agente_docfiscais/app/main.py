28a152f1f7ffe5847ba015377b60dd5d7de99c51
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-09-15 04:04:07 -0300

    agente nfs

--- Insurminds/Desafio 4/agente_docfiscais/app/main.py
+++ Insurminds/Desafio 4/agente_docfiscais/app/main.py
@@ -18,17 +18,6 @@ allow_origins=[
         "https://ENDERECO-PUBLICADO-DO-FRONTEND"
     ],"""
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "https://agente-nfs-574973424283.us-central1.run.app"
-    ],
-    allow_credentials=False,
-    allow_methods=["GET", "POST"],
-    allow_headers=["Content-Type"]
-)
 
 @app.get("/", response_class=HTMLResponse)
 async def frontpage():
