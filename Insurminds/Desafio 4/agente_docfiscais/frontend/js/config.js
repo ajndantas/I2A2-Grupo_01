561eae90bf79f74c8e7d5b6b31c2b401767a41ce
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-09-15 03:49:56 -0300

    agente nfs

--- Insurminds/Desafio 4/agente_docfiscais/frontend/js/config.js
+++ Insurminds/Desafio 4/agente_docfiscais/frontend/js/config.js
@@ -1,5 +1,6 @@
 export const CONFIG = {
-  apiBaseUrl: "http://127.0.0.1:8000",
+  //apiBaseUrl: "http://127.0.0.1:8000",
+  apiBaseUrl: "https://agente-nfs-574973424283.us-central1.run.app",
   demoMode: false,
   maxFileSize: 500 * 1024 * 1024,
   processingMessages: [
